bf73ce69981fcde04a508ffe55eecd50d932e6f9
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-11-25 13:27:37 -0300

    Minor css changes.

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -4,10 +4,10 @@ export default function Toggle(
 		{ label: string, toggled: boolean, onToggle: (e: MouseEvent) => void }) {
 	return (
 		<div>
-			<label class="relative inline-flex items-center cursor-pointer">
+			<label class="relative inline-flex items-center gap-2">
 				<input class="sr-only peer invisible" type="checkbox" checked={toggled} onClick={onToggle} />
 				<div class="w-11 h-6 bg-seagull-950 rounded-full after:absolute after:content-[''] after:aspect-square after:w-5 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full after:top-[2px] after:start-[2px] after:bg-seagull-50 after:rounded-full after:transition-transform peer-checked:bg-seagull-500"></div>
-				<span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">{label}</span>
+				<span class="text-sm font-medium text-gray-900 dark:text-gray-300">{label}</span>
 			</label>
 		</div>
 	);
