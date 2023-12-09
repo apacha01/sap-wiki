75aeb28216c2cd4d15fda2e221fdce0e88b020c5
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-12-08 23:31:11 -0300

    Create individual pack page. Implement some utilities to get extra info for the pack pets & foods.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,28 @@
+import { API_ENDPOINT } from 'src/constants/api';
+
 export const snakeCaseToNormalText = (text: string) => {
 	return text.replace(/_([a-z])/g, (_, match) => ` ${match.toUpperCase()}`).replace(/^[a-z]/, match => match.toUpperCase());
+};
+
+export const snakeCaseToUpperSnakeCase = (text: string) => {
+	return text.replace(/_([a-z])/g, (_, match) => `_${match.toUpperCase()}`).replace(/^[a-z]/, match => match.toUpperCase());
+};
+
+export const getEndpointModel = (url: string) => {
+	return url.substring(0, url.lastIndexOf('/')).replace(API_ENDPOINT, '');
+};
+
+export const getStandardSpriteUrl = (model:string, name: string) => {
+	return `https://raw.githubusercontent.com/apacha01/assets/sap/super-auto-pets/${model}/standard/${name}.png`;
+};
+
+export const getStandardImageUrlFromApiURL = (url: string, name: string) => {
+	const model = getEndpointModel(url);
+	return getStandardSpriteUrl(model, snakeCaseToUpperSnakeCase(name));
+};
+
+export const getWikiUriFromApiURL = (url: string) => {
+	const model = getEndpointModel(url);
+	const id = url.slice(url.lastIndexOf('/'));
+	return `/${model}/${id}`;
 };
