db08940da70a93becd657ce475a075c23481af46
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-12-12 02:06:03 -0300

    Fix: repetead '/' on api endpoints and add login.

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,13 +1,14 @@
 export const API_ENDPOINT = 'https://sapi-kkff.onrender.com/api/v1/';
 
-export const ALL_FOODS_ENDPOINT = API_ENDPOINT + '/foods';
-export const ALL_PETS_ENDPOINT = API_ENDPOINT + '/pets';
-export const ALL_PACKS_ENDPOINT = API_ENDPOINT + '/packs';
-export const ALL_TOKENS_ENDPOINT = API_ENDPOINT + '/tokens';
-export const ALL_TOYS_ENDPOINT = API_ENDPOINT + '/toys';
+export const ALL_FOODS_ENDPOINT = API_ENDPOINT + 'foods';
+export const ALL_PETS_ENDPOINT = API_ENDPOINT + 'pets';
+export const ALL_PACKS_ENDPOINT = API_ENDPOINT + 'packs';
+export const ALL_TOKENS_ENDPOINT = API_ENDPOINT + 'tokens';
+export const ALL_TOYS_ENDPOINT = API_ENDPOINT + 'toys';
+export const LOGIN_ENDPOINT = API_ENDPOINT + 'login';
 
-export const CUSTOM_FOOD_ENDPOINT = (name: string) => `${API_ENDPOINT}/foods/${name}`;
-export const CUSTOM_PET_ENDPOINT = (name: string) => `${API_ENDPOINT}/pets/${name}`;
-export const CUSTOM_PACK_ENDPOINT = (name: string) => `${API_ENDPOINT}/packs/${name}`;
-export const CUSTOM_TOKEN_ENDPOINT = (name: string) => `${API_ENDPOINT}/tokens/${name}`;
-export const CUSTOM_TOY_ENDPOINT = (name: string) => `${API_ENDPOINT}/toys/${name}`;
+export const CUSTOM_FOOD_ENDPOINT = (name: string) => `${API_ENDPOINT}${ALL_FOODS_ENDPOINT}/${name}`;
+export const CUSTOM_PET_ENDPOINT = (name: string) => `${API_ENDPOINT}${ALL_PETS_ENDPOINT}/${name}`;
+export const CUSTOM_PACK_ENDPOINT = (name: string) => `${API_ENDPOINT}${ALL_PACKS_ENDPOINT}/${name}`;
+export const CUSTOM_TOKEN_ENDPOINT = (name: string) => `${API_ENDPOINT}${ALL_TOKENS_ENDPOINT}/${name}`;
+export const CUSTOM_TOY_ENDPOINT = (name: string) => `${API_ENDPOINT}${ALL_TOYS_ENDPOINT}/${name}`;
