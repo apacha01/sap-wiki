18306f2b27a11c34d22f51ddb7a766cacd66e53a
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-12-12 03:17:08 -0300

    Fix: models endpoint.

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -7,8 +7,8 @@ export const ALL_TOKENS_ENDPOINT = API_ENDPOINT + 'tokens';
 export const ALL_TOYS_ENDPOINT = API_ENDPOINT + 'toys';
 export const LOGIN_ENDPOINT = API_ENDPOINT + 'login';
 
-export const CUSTOM_FOOD_ENDPOINT = (name: string) => `${API_ENDPOINT}${ALL_FOODS_ENDPOINT}/${name}`;
-export const CUSTOM_PET_ENDPOINT = (name: string) => `${API_ENDPOINT}${ALL_PETS_ENDPOINT}/${name}`;
-export const CUSTOM_PACK_ENDPOINT = (name: string) => `${API_ENDPOINT}${ALL_PACKS_ENDPOINT}/${name}`;
-export const CUSTOM_TOKEN_ENDPOINT = (name: string) => `${API_ENDPOINT}${ALL_TOKENS_ENDPOINT}/${name}`;
-export const CUSTOM_TOY_ENDPOINT = (name: string) => `${API_ENDPOINT}${ALL_TOYS_ENDPOINT}/${name}`;
+export const CUSTOM_FOOD_ENDPOINT = (name: string) => `${ALL_FOODS_ENDPOINT}/${name}`;
+export const CUSTOM_PET_ENDPOINT = (name: string) => `${ALL_PETS_ENDPOINT}/${name}`;
+export const CUSTOM_PACK_ENDPOINT = (name: string) => `${ALL_PACKS_ENDPOINT}/${name}`;
+export const CUSTOM_TOKEN_ENDPOINT = (name: string) => `${ALL_TOKENS_ENDPOINT}/${name}`;
+export const CUSTOM_TOY_ENDPOINT = (name: string) => `${ALL_TOYS_ENDPOINT}/${name}`;
