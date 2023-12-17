30716e40e2be04c0b20067b527cd62a1b100910a
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-12-17 01:03:04 -0300

    Change baseUrl in tsconfig to relative url.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()],
-  output: 'hybrid',
-  adapter: vercel()
+	integrations: [tailwind(), preact()],
+	output: 'hybrid',
+	adapter: vercel()
 });
